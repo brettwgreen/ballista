0de4fefa8ca056f120b4dbb861839c3a9ab9a14c
bgreen <bw_green@yahoo.com>
2016-05-22 10:59:51 -0400

    Fix facebook share button

--- src/ballista_facebook.js
+++ src/ballista_facebook.js
@@ -5,11 +5,9 @@ var ballista_facebook = (function ($, ballista) {
     };
     
     ballista.createFacebookButton = function(el, buttonType, options) {
-      var divRoot = "<div id='fb-root'></div>"
-      $(el).append(divRoot);      
-      ballista.loadFacebookScript();      
-
-      var div = "<div data-href='https://developers.facebook.com/docs/plugins/'";
+      var divroot = "<div id='fb-root'></div>"
+      $(el).append(divroot);      
+      var div = "<div";
       div += " class='" + buttonType + "'";
       for (var prop in options) {
         var val = options[prop];
@@ -19,12 +17,13 @@ var ballista_facebook = (function ($, ballista) {
       }
       div += "></div>";
       $(el).append(div);      
+      ballista.loadFacebookScript();	  
     };
-
+	   
     ballista.createFacebookShareButton = function(el, href, layout, useIFrame) {
       //<div class="fb-share-button" data-href="https://developers.facebook.com/docs/plugins/" data-layout="button_count" data-mobile-iframe="true"></div>
       var data = {};
-      data.layout = layout ? layout : "button-count";
+      data.layout = layout ? layout : "button_count";
       data["mobile-iframe"] = useIFrame ? useIFrame : true;
       data.href = href ? href : window.location.href;
       ballista.createFacebookButton(el, "fb-share-button", data);
